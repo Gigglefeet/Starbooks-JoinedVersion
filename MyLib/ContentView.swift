a17731205c42a0a990b346398b497039f472a268
robertmariuson <robert.mariuson@ebcont.com>
2025-04-19 14:52:07 +0200

    Initial Commit

---
+++ MyLib/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyLib
+//
+//  Created by Robert Mariuson on 19.04.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyLib/MyLibApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLibApp.swift
+//  MyLib
+//
+//  Created by Robert Mariuson on 19.04.25.
+//
+
+import SwiftUI
+
+@main
+struct MyLibApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
